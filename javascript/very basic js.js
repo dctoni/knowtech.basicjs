631ecf7a5b743541ed4290fb522371d8ffa2b784
dctoni <dctoni@gmail.com>
2015-11-16 18:23:08 +0100

    5a Clase 20151611

---
+++ javascript/very basic js.js
@@ -0,0 +1,12 @@
+/*
+
+En base a confirm(), prompt("Hola dime algo"), pregunta al usuario
+- COmo se llama
+- Que edad tiene
+- En base a la edad, muestra un texto que diga si es viejo o joven
+- y al final muesta una cadena que diga "Me llamo XXX, tengo xxx, 
+soy viejo o joven" 
+
+*/
+
+prompt("Hola dime algo");
--- user registration form/form.js
+++ user registration form/form.js
@@ -18,6 +18,10 @@
 			var tooltip = this.createTooltip(text);
 			var element = document.querySelector(elementSelector);
 
+			tooltip.id = element.id + "-tooltip";
+			var OtherTooltip = document.querySelector("#"+ tooltip.id);
+			OtherTooltip.parentNode.removeChild(OtherTooltip);
+
 			var leftPosition = element.offsetLeft;
 			var elementWidth = window.getComputedStyle(element).width;
 			elementWidth = elementWidth.replace("px", "");
@@ -31,14 +35,50 @@
 			var tooltipPosition = leftPosition + paddingLeft + paddingRight + elementWidth;
 
 			document.body.appendChild(tooltip);
-			tooltip.style.left = tooltipPosition + "px";
+			tooltip.style.left = tooltipPosition + 10 + "px";
+
+			var topPosition = element.offsetTop;
+			tooltip.style.top = topPosition + "px";
 		}
 	};
 
-	document.addEventListener("DOMContentLoaded", function(){
-	Form.appendTooltipNextTop("#userName", "Some stuff");
+	document.addEventListener("DOMContentLoaded", function() {
+
+		var registerMeButton = document.getElementById("registerUserButton");
+		
+		registerMeButton.addEventListener("click", function(e){
+			var userName = document.getElementById("userName").value;
+			if (userName == "" || userName == null) {
+				Form.appendTooltipNextTop("#userName", "Username no puede estar vacío");
+			} else {
+				Form.tooltip.style = "";
+			}
+
+			var password = document.getElementById("password").value;
+			if (password == "" || password == null) {
+				Form.appendTooltipNextTop("#password", "Password no puede estar vacío");
+			} else {
+				Form.tooltip.style = "";
+			}
+			
+			var repeatpassword = document.getElementById("repeatpassword").value;
+			if (repeatpassword == "" || repeatpassword == null) {
+				Form.appendTooltipNextTop("#repeatpassword", "Repeat Password no puede estar vacío");
+			} else {
+				Form.tooltip.style = "";
+			}
+
+			var email = document.getElementById("email").value;
+			if (email == "" || email == null) {
+				Form.appendTooltipNextTop("#email", "Email no puede estar vacío");
+			} else {
+				Form.tooltip.style = "";
+			}
+
+			e.preventDefault();
+
+		});
 
 	});
-	
 
 })();
